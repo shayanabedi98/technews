63b7ccb4e20d3aaf87593ce371a49521a48f7b8a
shayanabedi98 <shayanabedi@hotmail.com>
2024-06-28 21:13:57 -0400

    added next-auth functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Navbar() {
+  const { status } = useSession();
   return (
     <div className="flex justify-between pb-4 border-b mb-4">
       <div>
         <Link href={"/"}>
-          <h1 className="text-dark text-4xl font-bold tracking-tighter">Tech News</h1>
+          <h1 className="text-dark text-4xl font-bold tracking-tighter">
+            Tech News
+          </h1>
         </Link>
         <p className="text-sm">
           Exploring Tomorrow&apos;s Innovations, <br /> One Byte at a Time.
         </p>
       </div>
-      <div className="flex items-center">
-        <Link className="btn" href={"/sign-in"}>
-          Sign In
-        </Link>
-      </div>
+
+      {status === "authenticated" ? (
+        <div>
+          <button onClick={() => signOut()} className="btn">
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center">
+          <Link className="btn" href={"/sign-in"}>
+            Sign In
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
