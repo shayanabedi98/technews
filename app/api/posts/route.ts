bd9081ab9773510eaa563afed1c755b109678366
shayanabedi98 <shayanabedi@hotmail.com>
2024-07-08 21:15:21 -0400

    in process of creating apis for post and category

---
+++ app/api/posts/route.ts
@@ -0,0 +1,46 @@
+import prisma from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { title, content, links, selectedCategory, imageUrl, publicId } =
+    await req.json();
+  const authorEmail = "testing@email.com";
+
+  if (!title || !content) {
+    return NextResponse.json(
+      { error: "Title and content are required" },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        content,
+        links,
+        imageUrl,
+        publicId,
+        catName: selectedCategory,
+        authorEmail,
+      },
+    });
+    console.log("post created!");
+    return NextResponse.json(newPost);
+  } catch (error) {
+    return NextResponse.json({ message: "could not create post" });
+  }
+}
+
+export async function GET() {
+  try {
+    const posts = await prisma.post.findMany({
+      include: { author: { select: { name: true } } },
+      orderBy: { createdAt: "desc" },
+    });
+    return NextResponse.json(posts);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "some error occured" });
+  }
+}
